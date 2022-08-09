e015e4360bb761ad22ffeaee8cd36a3fe11f9176
davejang <davejang98@naver.com>
2022-08-09 22:54:00 +0900

    Audio Record

---
+++ components/AudioRecord.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { Button, StyleSheet, StatusBar, Title, Text, View } from 'react-native';
+import { Audio } from 'expo-av'
+
+function AudioRecord() {
+  const [recording, setRecording] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+         Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI(); 
+    console.log('Recording stopped and stored at', uri);
+  }
+  
+  return (
+      <View style={styles.container}>
+        <Button
+          title={recording ? 'Stop Recording' : 'Start Recording'}
+          onPress={recording ? stopRecording : startRecording}
+        />
+      <StatusBar style="auto" />
+    </View>           
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+export default AudioRecord;
